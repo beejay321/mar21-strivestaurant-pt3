427c1b32d0fef7077f8be2abc9b9fed42fb2b5ab
beejay321 <jibodu.busola@gmail.com>
2021-05-04 14:27:48 +0200

    Dish comments update

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -1,17 +1,22 @@
-import { ListGroup } from 'react-bootstrap'
+import React from "react";
+import { ListGroup } from "react-bootstrap";
 
-const DishComments = (props) => (
-    // props.dish is the whole object
-    <div className={`mt-${props.marginTop}`}>
-        <h2>Comments for {props.dish.name}</h2>
+class DishComments extends React.Component {
+  render() {
+    return (
+      // props.dish is the whole object
+      <div className={`mt-${this.props.marginTop}`}>
+        <h2>Comments for {this.props.dish.name}</h2>
         <ListGroup>
-            {
-                props.dish.comments.map(c => (
-                    <ListGroup.Item key={c.id}>"{c.comment}" from {c.author}</ListGroup.Item>
-                ))
-            }
+          {this.props.dish.comments.map((c) => (
+            <ListGroup.Item key={c.id}>
+              "{c.comment}" from {c.author}
+            </ListGroup.Item>
+          ))}
         </ListGroup>
-    </div>
-)
+      </div>
+    );
+  }
+}
 
-export default DishComments
+export default DishComments;
